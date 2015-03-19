fa06dfe2214f438c13298f899f003076a1daec9a
DiiSandoval <diimieres@gmail.com>
2015-03-19 18:50:07 +0100

    Select example working

---
+++ src/main/java/uniovi/innova/classes/SelectOneView.java
@@ -0,0 +1,17 @@
+package uniovi.innova.classes;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class SelectOneView {
+     
+    private String option;
+     
+    public String getOption() {
+        return option;
+    }
+ 
+    public void setOption(String option) {
+        this.option = option;
+    }
+}
